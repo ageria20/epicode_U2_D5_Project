71e10777ef58f17591d96f49ee65d0a950193624
Andrea <ageria20@gmail.com>
2024-05-31 22:12:44 +0200

    aside fissato

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -3,11 +3,9 @@ const buttonNav = document.querySelector("#buttonNav");
 
 window.onscroll = function () {
   if (window.scrollY >= 400) {
-    console.log("ciao");
     scrollNav.style.backgroundColor = "white";
     buttonNav.style.backgroundColor = "green";
   } else {
-    console.log("no bueno");
     scrollNav.style.backgroundColor = "#ffc017";
     buttonNav.style.backgroundColor = "black";
   }
